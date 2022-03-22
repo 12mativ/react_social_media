1ca31b5355c44c5a586e98b55cf24edf28115f6b
mativ <ivan_m146@mail.ru>
2022-03-22 19:19:01 +0400

    3 components, few css

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src="https://user-images.githubusercontent.com/88069082/149673405-b47f71c1-0777-4038-9247-5b0c9d166f22.jpg" alt=""/>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
